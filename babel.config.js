6c56b32fc662222d140d1e9f17dbb9b8f5433a37
Alejandro Sánchez <alejandroslpz@gmail.com>
2023-01-18 08:46:09 -0600

    chore: Module-resolver config for ts & babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "~api": "./src/api",
+            "~assets": "./src/assets",
+            "~components": "./src/components",
+            "~context": ["src/context"],
+            "~features": "./src/features",
+            "~hooks": "./src/hooks",
+            "~themes": "./src/themes",
+          },
+        },
+      ],
+    ],
   };
 };
