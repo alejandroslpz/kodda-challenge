047138643d021fec57b823f3068a4ca18b95b67b
Alejandro Sánchez <alejandroslpz@gmail.com>
2023-01-18 09:03:07 -0600

    feat: Default theme for react navigation

---
+++ src/themes/theme.ts
@@ -0,0 +1,13 @@
+import type { Theme } from "@react-navigation/native";
+
+export const theme: Theme = {
+  dark: false,
+  colors: {
+    primary: "#8F03CC",
+    background: "#ffffff",
+    card: "#ffffff",
+    text: "#000000",
+    border: "#000000",
+    notification: "#8F03CC",
+  },
+};
